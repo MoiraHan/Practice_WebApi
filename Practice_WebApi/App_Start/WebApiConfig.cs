9b7dc67ef32e74bc8fbd7c37bc9e3ccd1c3e4f40
moira_han <moira_han@mail.csf.org.tw>
2020-05-08 18:14:48 +0800

    * 建立 WebApi、WebSite 專案，使用北風資料庫，練習從 Api 接資料

---
+++ Practice_WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Practice_WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 設定和服務
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // 如果 Json 序列化是使用 Json.Net 序列化 ( 應該是微軟原生的Json(?) )，
+            // 這行程式可以忽略循環引用 ( 就是 Model 中有相同類型的 Property，導致進行 Json 序列化時無限迴圈 )
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+    }
+}
---
+++ Practice_WebApi/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Practice_WebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // 關閉 XML 格式回應，就會預設回應 Json 格式
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+        }
+    }
+}
